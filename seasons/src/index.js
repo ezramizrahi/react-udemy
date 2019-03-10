364fa1b98432e0957ba3a7117dee83fcf507527a
ezra mizrahi <mishraki@gmail.com>
2019-03-10 23:52:34 +0800

    loading component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   constructor(props) {
@@ -28,15 +29,15 @@ class App extends React.Component {
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     if (!this.state.errorMessage && !this.state.lat) {
-      return <div>Loading your latitude!</div>
+      return <Spinner />;
     }
   }
 }
