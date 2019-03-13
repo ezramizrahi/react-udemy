93548d533f7b8ff3cdcc852de207891f147edd57
ezra mizrahi <mishraki@gmail.com>
2019-03-13 23:10:40 +0800

    tickling the endpoint

--- pikshuz/src/components/App.js
+++ pikshuz/src/components/App.js
@@ -3,14 +3,30 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
 
+  state = {
+    images: []
+  };
+
+  onSearchSubmit =  async (term) => {
+    const response = await axios
+      .get(`https://api.unsplash.com/search/photos`, {
+        params: {
+          query: term
+        },
+        headers: {
+          Authorization: `Client-ID 8aedfd6feeeebaccf7ca7634748904e6ffbef1879cde3b83b61b3b82f2e06dfc`
+        }
+      });
+
+      this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
       </div>
     );
   }
