472cee62621a3c26f0da3f061a5d1b5adcd9eb3e
ezra mizrahi <mishraki@gmail.com>
2019-03-14 11:35:54 +0800

    destructuring

--- pikshuz/src/components/ImageList.js
+++ pikshuz/src/components/ImageList.js
@@ -1,11 +1,12 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
 
-  const images = props.images.map(({ description, id, urls }) => {
+  const images = props.images.map((image) => {
     return (
-      <img key={id} src={urls.regular} alt={description} />
+      <ImageCard key={image.id} image={image} />
     )
   });
 
