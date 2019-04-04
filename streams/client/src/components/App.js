640704fa9154664877fa0c8fee572ad77f623e46
ezra mizrahi <mishraki@gmail.com>
2019-04-04 12:36:58 +0800

    created components and imported

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
